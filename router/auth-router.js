814443f02e0d85ed2a3a726d1f1c2bf967027aa9
Rishabh <rishabhbhatt956@gmail.com>
2024-12-03 13:09:32 +0530

    Controllers in Express.js

--- router/auth-router.js
+++ router/auth-router.js
@@ -1,17 +1,10 @@
 const express = require("express")
 const router = express.Router()
+const authcontrollers = require('../controllers/atuh-controllers')
 
 
-router.get("/", (req, res)=>{
-    res.status(200).send("This is our server")
-})
+router.route("/").get(authcontrollers.home)
 
-router.get("/about", (req, res)=>{
-    res.status(200).send("This is about page lol")
-})
-
-router.route("/register").get((req, res)=>{
-    res.send(200).send("THis is our register app")
-})
+router.route("/register").get(authcontrollers.register)
 
 module.exports = router;
